fd0cb52946f61b8a80c09b4209cf3eb04c512c16
Neko12138 <wxu78@ucsc.edu>
2025-05-11 13:01:06 -0700

    done

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -51,19 +51,20 @@ class Platformer extends Phaser.Scene {
     update() {
         if(cursors.left.isDown) {
             // TODO: have the player accelerate to the left
-            
+            my.sprite.player.body.setAccelerationX(-this.ACCELERATION);
             my.sprite.player.resetFlip();
             my.sprite.player.anims.play('walk', true);
 
         } else if(cursors.right.isDown) {
             // TODO: have the player accelerate to the right
-
+            my.sprite.player.body.setAccelerationX(this.ACCELERATION);
             my.sprite.player.setFlip(true, false);
             my.sprite.player.anims.play('walk', true);
 
         } else {
             // TODO: set acceleration to 0 and have DRAG take over
-
+            my.sprite.player.body.setAccelerationX(0);
+            my.sprite.player.body.setDragX(this.DRAG);
             my.sprite.player.anims.play('idle');
         }
 
@@ -73,6 +74,7 @@ class Platformer extends Phaser.Scene {
             my.sprite.player.anims.play('jump');
         }
         if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+            my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
             // TODO: set a Y velocity to have the player "jump" upwards (negative Y direction)
 
         }
